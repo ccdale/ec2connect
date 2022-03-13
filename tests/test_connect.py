03249076d328388313c67af1b2d76a8394b87975
ccdale <chris.allison@hivehome.com>
2022-03-13 02:34:11 +0000

    updating tests

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -2,7 +2,27 @@
 
 import os
 
+from botocore.config import Config
+from moto import mock_ec2
+import pytest
+
 from ec2connect.connect import mkSession
+
+config = Config(
+    region_name="eu-west-1",
+    signature_version="v4",
+    retries={"max_attempts": 10, "mode": "standard"},
+)
+
+
+@pytest.fixture(scope="function")
+def aws_creds():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu_west_1"
 
 
 def test_mkSession_no_env():
@@ -12,7 +32,17 @@ def test_mkSession_no_env():
 
 
 def test_mkSession_with_profile():
-    """tests that mkSession can set the profile"""
+    """tests that mkSession can set the profile
+
+    relies on a profile of this name existing
+    in either of the ~/.aws/{config, credentials} files
+
+    it can be blank as in
+
+    [experiments-sre]
+    # blank profile
+
+    """
     sess = mkSession(profile="experiments-sre")
     assert sess.profile_name == "experiments-sre"
 
@@ -24,7 +54,10 @@ def test_mkSession_set_region():
 
 
 def test_mkSession_env_override():
-    """tests that mkSession doesn't overwrite the environment if set"""
+    """tests that mkSession doesn't overwrite the environment if set
+
+    ensure a 2nd blank profile called 'wibble' is also setup
+    """
     os.environ["AWS_PROFILE"] = "wibble"
     sess = mkSession(profile="experiments-sre")
     assert sess.profile_name == "wibble"
